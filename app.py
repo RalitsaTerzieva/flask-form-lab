e5c3ff2587802f6c60e6ec59f75bd0d3dc4482a3
Ralica Popova <ralicaterzieva90@gmail.com>
2024-11-21 18:09:43 +0200

    Add more routes

--- app.py
+++ app.py
@@ -24,10 +24,19 @@ def index():
     form = InfoForm()
 
     if form.validate_on_submit():
-        breed = form.breed.data
-        form.breed.data = ''
+        session['breed'] = form.breed.data
+        session['neutered'] = form.neutered.data
+        session['mood'] = form.mood.data
+        session['food'] = form.food_choice.data
+        session['feedback'] = form.feedback.data
+       
+        return redirect(url_for('thankyou'))
 
     return render_template('index.html', form=form, breed=breed)
+
+@app.route('/thank-you')
+def thankyou():
+    return render_template('thankyou.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
